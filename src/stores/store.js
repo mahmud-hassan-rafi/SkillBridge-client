9b6b400b782b8b14ed763cbd51ded7aff7a099d9
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-20 23:35:44 +0600

    create redux store and add rtk configs

---
+++ src/stores/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "@services/api.js";
+import authReducer from "@features/auth/authSlice.js";
+
+export const store = configureStore({
+  name: "SkillBridgeStore",
+  reducer: {
+    auth: authReducer,
+    [api.reducerPath]: api.reducer,
+  },
+
+  middleware: (gDM) => gDM().concat(api.middleware),
+});
