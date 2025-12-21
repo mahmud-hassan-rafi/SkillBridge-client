ebe31311e9aa4b7d6b49e305f6e88a39ce9cfbf2
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-21 22:47:13 +0600

    added success and error notify

---
+++ src/utils/toast-notify/toastify.js
@@ -0,0 +1,23 @@
+import { toast } from "react-toastify";
+
+export const successNotify = (message) => {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    className: "text-sm",
+  });
+};
+
+export const errorNotify = (message) => {
+  toast.error(message, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    className: "text-sm",
+  });
+};
