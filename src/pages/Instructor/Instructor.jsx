bc7b2de768924e6ec273438827a67d63470fa255
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-23 09:15:40 +0600

    feat: add instructor initial layout page

---
+++ src/pages/Instructor/Instructor.jsx
@@ -0,0 +1,22 @@
+import Footer from "@components/Instructor/Footer";
+import Navbar from "@components/Instructor/Navbar";
+import Sidebar from "@components/Instructor/Sidebar";
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Instructor = () => {
+  return (
+    <div className="text-base min-h-screen bg-white">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 overflow-y-auto">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Instructor;
