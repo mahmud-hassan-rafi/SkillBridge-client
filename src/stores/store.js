b9111c22a4e1064c2b5a0de8bc1b9ae3a1c28a9b
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-31 00:11:09 +0600

    feat: add meReducer to the Redux store configuration

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "@services/api.js";
 import authReducer from "@features/auth/authSlice.js";
+import meReducer from "@features/me/meSlice.js";
 
 export const store = configureStore({
   name: "SkillBridgeStore",
   reducer: {
     auth: authReducer,
+    me: meReducer,
     [api.reducerPath]: api.reducer,
   },
 
