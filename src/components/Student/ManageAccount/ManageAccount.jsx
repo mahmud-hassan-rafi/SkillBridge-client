b13897632d5c3974519432ed2df86a0ab324c67c
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-02 00:47:32 +0600

    feat: update layout styles for ManageAccount component

--- src/components/Student/ManageAccount/ManageAccount.jsx
+++ src/components/Student/ManageAccount/ManageAccount.jsx
@@ -16,7 +16,7 @@ const ManageAccount = () => {
   return (
     <div className="fixed z-20 inset-0 bg-black/70 flex items-center justify-center">
       <div
-        className="bg-white rounded-lg flex md:min-w-[60vw] md:h-[90vh] z-25 "
+        className="bg-white rounded-lg flex min-w-[90vw] md:min-w-[80vw] xl:min-w-[60vw] min-h-[90vh] z-25 "
         onClick={(e) => {
           e.stopPropagation();
         }}
