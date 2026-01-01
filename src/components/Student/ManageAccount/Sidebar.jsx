f21477624edce13cef16954074076304c1fb244e
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-02 00:48:28 +0600

    feat: update ManageAccountNavbar styles for improved responsiveness and layout

--- src/components/Student/ManageAccount/Sidebar.jsx
+++ src/components/Student/ManageAccount/Sidebar.jsx
@@ -4,33 +4,33 @@ import { MdOutlineSecurity } from "react-icons/md";
 
 const ManageAccountNavbar = ({ clickedOn, setClickedOn }) => {
   return (
-    <div className="w-[30%] bg-gray-200/60 p-4 flex flex-col gap-2">
-      <div className="flex flex-col mb-4">
+    <div className="md:w-[27%] min-w-15 bg-gray-200/60 md:p-4 flex flex-col gap-2">
+      <div className="hidden md:flex flex-col mb-4">
         <h2 className="text-2xl font-semibold">Account</h2>
         <h4 className="text-sm text-gray-700/70">Manage your account info</h4>
       </div>
       <div className="flex flex-col">
         <span
           onClick={() => setClickedOn("profile")}
-          className={`flex gap-3 w-full p-2 items-center rounded  ${
+          className={`flex gap-3 w-full p-2 max-md:py-3 items-center max-md:justify-center rounded  ${
             clickedOn === "profile"
               ? "bg-black/10 cursor-default"
               : "text-gray-900/70 cursor-pointer"
           }`}
         >
-          <FaUserCircle />
-          <p className="text-sm">Profile</p>
+          <FaUserCircle className="text-2xl md:text-sm" />
+          <p className="text-sm hidden md:flex">Profile</p>
         </span>
         <span
           onClick={() => setClickedOn("security")}
-          className={`flex gap-3 w-full p-2 items-center rounded ${
+          className={`flex gap-3 w-full p-2 max-md:py-3 items-center max-md:justify-center rounded ${
             clickedOn === "security"
               ? "bg-black/10 cursor-default"
               : "text-gray-900/70 cursor-pointer"
           }`}
         >
-          <MdOutlineSecurity />
-          <p className="text-sm">Security</p>
+          <MdOutlineSecurity className="text-2xl md:text-sm" />
+          <p className="text-sm hidden md:flex">Security</p>
         </span>
       </div>
     </div>
