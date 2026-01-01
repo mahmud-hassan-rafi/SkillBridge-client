1db84c78c1cc2b5bf65b4383e8338aa93ff74a21
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-01 12:07:55 +0600

    feat: add Details component for managing user profile and security settings

---
+++ src/components/Student/ManageAccount/Details.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ProfileDetails from "./ProfileDetails";
+import SecurityDetails from "./SecurityDetails";
+import { assets } from "@assets/assets";
+import { useAppContext } from "@context/AppContext";
+
+const Details = ({ clickedOn }) => {
+  const { setManageAccount } = useAppContext();
+
+  return (
+    <div className="w-[70%] p-5 relative">
+      <img
+        src={assets.cross_icon}
+        alt=""
+        className="absolute top-5 right-5 size-3 cursor-pointer"
+        onClick={() => setManageAccount(false)}
+      />
+      {clickedOn === "profile" ? <ProfileDetails /> : <SecurityDetails />}
+    </div>
+  );
+};
+
+export default Details;
