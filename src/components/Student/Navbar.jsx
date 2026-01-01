3b0caddb6d88cc70b3347e815ca4eb8c0b6b30dd
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-01 12:06:45 +0600

    feat: enhance Navbar with profile management features

--- src/components/Student/Navbar.jsx
+++ src/components/Student/Navbar.jsx
@@ -1,14 +1,46 @@
 import { assets } from "@assets/assets";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import Profile from "./Profile";
+import ManageAccount from "./ManageAccount/ManageAccount";
+import { useAppContext } from "@context/AppContext";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const auth = useSelector((state) => state?.auth);
+
   const isCourseListPath = location.pathname.includes("/course-list");
   const isLoginPath = location.pathname.includes("/login");
   const isSignUpPath = location.pathname.includes("/signup");
-  const navigate = useNavigate();
-  const auth = useSelector((state) => state?.auth);
+
+  const profilePictureRef = useRef();
+  const [profilePosition, setProfilePosition] = useState(null);
+  const [clickOnProfile, setClickOnProfile] = useState(false);
+
+  const { manageAccount, setManageAccount } = useAppContext();
+  // profile picture
+  const onClickHandler = () => {
+    const rect = profilePictureRef.current.getBoundingClientRect();
+
+    setProfilePosition({
+      top: rect.top,
+      bottom: rect.bottom,
+      right: rect.right,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height,
+      x: rect.x,
+      y: rect.y,
+    });
+    console.log(rect);
+    setTimeout(() => setClickOnProfile((prev) => !prev), 200);
+  };
+
+  window.addEventListener("scroll", () => {
+    setClickOnProfile(false);
+  });
+
   return (
     <div
       className={`flex items-center justify-between px-2 xs:px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
@@ -54,8 +86,18 @@ const Navbar = () => {
           src={auth?.user?.imageUrl}
           alt=""
           className="w-10 h-10 rounded-full ml-5 cursor-pointer"
+          onClick={onClickHandler}
+          ref={profilePictureRef}
         />
       </div>
+      {clickOnProfile && (
+        <Profile
+          profilePosition={profilePosition}
+          setClickOnProfile={setClickOnProfile}
+          setManageAccount={setManageAccount}
+        />
+      )}
+      {manageAccount && <ManageAccount />}
       {/* mobile view */}
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
         {auth?.isAuthenticated ? (
