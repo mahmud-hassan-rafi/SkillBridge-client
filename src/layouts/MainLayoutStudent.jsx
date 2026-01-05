67ceaa17413b44d76cdb9d428e6678ee3ee907d0
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-06 00:10:56 +0600

    remove: move toastContainer

--- src/layouts/MainLayoutStudent.jsx
+++ src/layouts/MainLayoutStudent.jsx
@@ -1,14 +1,12 @@
 import Navbar from "@components/Student/Navbar";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 
 const MainLayoutStudent = () => {
   return (
     <div className="text-default min-h-screen bg-white">
       <Navbar />
       <Outlet />
-      <ToastContainer />
     </div>
   );
 };
