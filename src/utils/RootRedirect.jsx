c7d754b7090bf5c6cbe49af5954510c495cb79e3
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-05 23:56:20 +0600

    feat: redirect users from root based on role and enrollment state

--- src/utils/RootRedirect.jsx
+++ src/utils/RootRedirect.jsx
@@ -1,23 +1,26 @@
-// import React, { useEffect } from "react";
-// import { useSelector } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import Instructor from "@pages/Instructor/Instructor";
+import Home from "@pages/Student/Home";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-// const RootRedirect = ({ isLoading }) => {
-//   const navigate = useNavigate();
-//   const user = useSelector((state) => state?.auth?.user);
-//   useEffect(() => {
-//     if (isLoading) return;
+const RootRedirect = ({ isLoading }) => {
+  const navigate = useNavigate();
+  const user = useSelector((state) => state?.auth?.user);
 
-//     if (!user) {
-//       navigate("/");
-//     } else if (user?.role === "user") {
-//       navigate("/user");
-//     } else if (user?.role === "captain") {
-//       navigate("/captain");
-//     }
-//   }, [user, isLoading, navigate]);
+  useEffect(() => {
+    if (isLoading) return;
+    if (
+      !user ||
+      (user?.enrolledCourses.length === 0 && user?.role === "student")
+    ) {
+      navigate("/home");
+    } else if (user?.role === "instructor") {
+      navigate("/instructor");
+    }
+  }, [navigate, user, isLoading]);
 
-//   return <div>redirecting...</div>;
-// };
+  return <div>redirecting...</div>;
+};
 
-// export default RootRedirect;
+export default RootRedirect;
