9c8ce2e6d0275025ba879ccf0cd64aa86f1c8167
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-20 21:20:35 +0600

    feat: add GlobalImpact and InstructorLayout components

---
+++ src/components/Instructor/becomeInstructor/GlobalImpact.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const GlobalImpact = () => {
+  const repeatedContent = [
+    {
+      number: "100K",
+      title: "Students",
+    },
+    {
+      number: "20+",
+      title: "Languages",
+    },
+    {
+      number: "250K",
+      title: "Enrollments",
+    },
+    {
+      number: "68+",
+      title: "Countries",
+    },
+    {
+      number: "700+",
+      title: "Enterprise customers",
+    },
+  ];
+
+  return (
+    <div className="flex md:justify-between justify-center lg:p-16 md:p-12 p-6 px-0 flex-wrap gap-3 sm:gap-5  md:gap-10 bg-[#5022c3] text-white">
+      {repeatedContent.map((content, idx) => (
+        <span key={idx} className="p-4">
+          <h1 className="text-4xl md:text-5xl font-semibold text-center">
+            {content.number}
+          </h1>
+          <p className="text-center">{content.title}</p>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default GlobalImpact;
