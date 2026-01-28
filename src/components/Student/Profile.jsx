e242c4bdcdc8ec05d099d475ad51c93cd5a3cd13
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-01-28 21:10:33 +0600

    feat: implement confirmation dialog for logout action in Profile component

--- src/components/Student/Profile.jsx
+++ src/components/Student/Profile.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { IoMdSettings } from "react-icons/io";
 import { PiSignOut } from "react-icons/pi";
 import { useAppContext } from "@context/AppContext";
 import { useLogoutMutation } from "@features/auth/authApi";
 import { useNavigate } from "react-router-dom";
+import { RxCross2 } from "react-icons/rx";
+import { MdDone } from "react-icons/md";
 
 const Profile = ({ setClickOnProfile, profilePosition, setManageAccount }) => {
-  const [logout] = useLogoutMutation();
   const navigate = useNavigate();
-
+  const [logout] = useLogoutMutation();
+  const [clickOnLogout, setClickOnLogout] = useState(false);
   const user = useSelector((state) => state.auth?.user);
   const { capitalize } = useAppContext();
 
@@ -65,19 +67,50 @@ const Profile = ({ setClickOnProfile, profilePosition, setManageAccount }) => {
 
         {/* signout */}
         <div
-          className="flex items-center gap-4 px-3 py-4 hover:bg-black/5 w-full cursor-pointer transition-colors "
+          className="w-full"
           onClick={() => {
-            logout();
-            setClickOnProfile(false);
-            navigate("/");
+            setClickOnLogout(true);
           }}
         >
-          <span className="flex w-[15%] items-center justify-center">
-            <PiSignOut className="h-5 w-5 text-gray-800/80 " />
-          </span>
-          <span className="flex flex-col w-[85%]">
-            <h2 className="text-base font-normal text-gray-900/70">Sign out</h2>
-          </span>
+          {clickOnLogout ? (
+            // wanted to logout
+            <div className="flex items-center gap-5 px-5 w-full py-4">
+              <span className="px-5 text-lg font-light text-gray-800/80">
+                Are you sure?
+              </span>
+              <span className="flex gap-5">
+                {/* tick icon */}
+                <MdDone
+                  className="size-5 text-green-800/80 cursor-pointer rounded-full hover:bg-black/5"
+                  onClick={() => {
+                    logout();
+                    setClickOnProfile(false);
+                    navigate("/");
+                  }}
+                />
+                {/* cross icon */}
+                <RxCross2
+                  className="size-5 text-red-600/80 cursor-pointer rounded-full hover:bg-black/5"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setClickOnLogout(false);
+                  }}
+                />
+              </span>
+            </div>
+          ) : (
+            // logout tab
+            <div className=" flex items-center gap-4 px-3 py-4 w-full cursor-pointer transition-colors hover:bg-black/5">
+              <span className="flex w-[15%] items-center justify-center ">
+                <PiSignOut className="h-5 w-5 text-gray-800/80 " />
+              </span>
+              <span className="flex flex-col w-[85%]">
+                <h2 className="text-base font-normal text-gray-900/70">
+                  Sign out
+                </h2>
+              </span>
+            </div>
+          )}
         </div>
       </div>
     </div>
