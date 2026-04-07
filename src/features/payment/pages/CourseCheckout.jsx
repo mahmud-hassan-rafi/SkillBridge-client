a393180fafa9a047ca49f9d727e6909742a2c229
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-07 21:10:53 +0600

    feat: add CourseCheckout and PaymentSuccess components for handling course payments

---
+++ src/features/payment/pages/CourseCheckout.jsx
@@ -0,0 +1,55 @@
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+import Loading from "@components/common/Loading";
+import CheckoutForm from "@features/payment/components/checkoutForm/CheckoutForm";
+import CourseCard from "../components/courseCard/CourseCard";
+import { useAppContext } from "@context/AppContext";
+import { useParams } from "react-router-dom";
+import CourseCardSkeleton from "@skeleton/course/CourseCardSkeleton";
+
+let stripePromise;
+
+try {
+  stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
+} catch (error) {
+  console.log(error);
+}
+
+export default function CourseCheckout() {
+  const { allCourses } = useAppContext();
+  const { id } = useParams();
+
+  const courseData = allCourses.find((course) => course._id === id);
+  const clientSecret = useSelector((state) => state.payment.clientSecret);
+  console.log(clientSecret);
+
+  if (!clientSecret) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="w-full p-6 sm:px-8 xl:px-16 bg-linear-to-b from-cyan-100/70">
+      {clientSecret && (
+        <div className="w-full flex max-[850px]:flex-col max-[850px]:items-center min-[850px]:gap-8 md:justify-around">
+          <CourseCard courseData={courseData} />
+          <Elements stripe={stripePromise} options={{ clientSecret }}>
+            <CheckoutForm id={id} />
+          </Elements>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// loader data use kora plan ache after building backend.
+// export const CourseCheckoutLoader = async ({params}) => {
+//   const {id} = params;
+//   const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/courses/${id}`);
+
+//   if (!res.ok) {
+//     throw new Error("course loaded failed");
+//   }
+
+//   return res.json();
+// }
