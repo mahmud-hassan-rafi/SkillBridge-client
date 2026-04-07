ac28a81d85a06568638ef502a706ef3bc55ddb14
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-07 20:55:02 +0600

    feat: enhance CourseCard component with lazy loading and loading state

--- src/components/Student/CourseCard.jsx
+++ src/components/Student/CourseCard.jsx
@@ -1,17 +1,34 @@
 import { assets } from "@assets/assets";
 import { useAppContext } from "@context/AppContext";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
 
 const CourseCard = ({ course }) => {
+  const [loaded, setLoaded] = useState(false);
   const { currency, calculateAvarageRating } = useAppContext();
+
+  const handleOnload = useCallback(() => {
+    setLoaded(true);
+  }, [setLoaded]);
 
   return (
     <Link
       to={"/course/" + course._id}
-      className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg"
+      className="border border-gray-500/30 pb-6 overflow-hidden rounded-lg flex flex-col"
     >
-      <img className="w-full" src={course.courseThumbnail} alt="" />
+      {
+        <div className="relative h-auto">
+          {!loaded && <div className="w-full bg-gray-200 animate-pulse h-35" />}
+
+          <img
+            loading="lazy"
+            onLoad={handleOnload}
+            className={`w-full h-min object-fit object-center ${loaded ? "opacity-100" : "opacity-0"}`}
+            src={course.courseThumbnail}
+            alt=""
+          />
+        </div>
+      }
       <div className="p-3 text-left">
         <h3 className="font-semibold text-base line-clamp-2">
           {course.courseTitle}
@@ -22,6 +39,7 @@ const CourseCard = ({ course }) => {
           <div className="flex">
             {[...Array(5)].map((_, idx) => (
               <img
+                loading="lazy"
                 className="w-3.5 h-3.5"
                 key={idx}
                 src={
