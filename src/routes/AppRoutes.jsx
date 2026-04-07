650833ff30321f406daa683a5d7f0e9e7c35936c
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-07 21:08:44 +0600

    feat: implement InstructorNotAllowed guard and lazy load components in AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "@pages/Student/Home";
 import CourseDetails from "@pages/Student/CourseDetails";
-import CoursesList from "@pages/Student/CoursesList";
+const CoursesList = lazy(() => import("@pages/Student/CoursesList"));
 import MyEnrollments from "@pages/Student/MyEnrollments";
 import Player from "@pages/Student/Player";
 import Dashboard from "@pages/Instructor/Dashboard";
@@ -13,10 +13,10 @@ import StudentsEnrolled from "@pages/Instructor/StudentsEnrolled";
 import MainLayoutStudent from "@layouts/MainLayoutStudent";
 import PageNotFound from "@pages/common/PageNotFound";
 import BecomeInstructor from "@pages/Instructor/BecomeInstructor";
-import Login from "@pages/Student/Login";
-import Signup from "@pages/Student/Signup";
-import PrivateRoutes from "@components/common/PrivateRoutes";
-import RootRedirect from "@utils/RootRedirect";
+import Login from "@features/auth/pages/Login";
+import Signup from "@features/auth/pages/Signup";
+import PrivateRoutes from "@routes/guards/PrivateRoutes";
+import RootRedirect from "@routes/redirects/RootRedirect";
 import InstructorLayout from "@layouts/InstructorLayout";
 import BecomeInstructorOnboardingLayout from "@layouts/BecomeInstructorOnboardingLayout";
 import TeachingExperienceOnboarding from "@pages/Instructor/become-instructor/onboarding/TeachingExperienceOnboarding";
@@ -24,25 +24,60 @@ import VideoExperienceOnboarding from "@pages/Instructor/become-instructor/onboa
 import ExistingAudienceOnboarding from "@pages/Instructor/become-instructor/onboarding/ExistingAudienceOnboarding";
 import { InstructorOnBoardingContextProvider } from "@context/InstructorOnBoarding.context";
 import Loading from "@components/common/Loading";
-import CourseCheckout from "@pages/Student/CourseCheckout";
-import PaymentSuccess from "@pages/Student/PaymentSuccess";
+const CourseCheckout = lazy(
+  () => import("@features/payment/pages/CourseCheckout"),
+);
+import PaymentSuccess from "@features/payment/pages/PaymentSuccess";
+import InstructorNotAllowed from "./guards/NotAllowed";
 
 const AppRoutes = ({ isLoading }) => {
   return (
     <Routes>
       <Route path="/" element={<MainLayoutStudent />}>
         <Route index={true} element={<RootRedirect isLoading={isLoading} />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <InstructorNotAllowed>
+              <Home />
+            </InstructorNotAllowed>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/course-list" element={<CoursesList />} />
-        <Route path="/course-list/:input" element={<CoursesList />} />
-        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route
+          path="/course-list"
+          element={
+            <InstructorNotAllowed>
+              <Suspense fallback={<Loading />}>
+                <CoursesList />
+              </Suspense>
+            </InstructorNotAllowed>
+          }
+        />
+        <Route
+          path="/course-list/:input"
+          element={
+            <InstructorNotAllowed>
+              <CoursesList />
+            </InstructorNotAllowed>
+          }
+        />
+        <Route
+          path="/course/:id"
+          element={
+            <InstructorNotAllowed>
+              <CourseDetails />
+            </InstructorNotAllowed>
+          }
+        />
         <Route
           path="/course/enroll/:id"
           element={
             <PrivateRoutes allowRoles={"student"} route={"/login"}>
-              <CourseCheckout />
+              <Suspense fallback={<Loading />}>
+                <CourseCheckout />
+              </Suspense>
             </PrivateRoutes>
           }
         />
