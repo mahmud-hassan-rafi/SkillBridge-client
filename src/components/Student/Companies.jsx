f2b7d281c4078fe5ab6353dbaa6693d8320c5472
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-23 18:25:51 +0600

    feat: refactor Companies component to use CompanyLogo for image rendering

--- src/components/Student/Companies.jsx
+++ src/components/Student/Companies.jsx
@@ -1,6 +1,7 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { assets } from "@assets/assets";
 import uniqid from "uniqid";
+import CompanyLogo from "./home/CompanyLogo";
 
 const srcList = [
   {
@@ -26,29 +27,12 @@ const srcList = [
 ];
 
 const Companies = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  const handleOnload = useCallback(() => {
-    return setIsLoaded(true);
-  }, [setIsLoaded]);
-
   return (
     <div className="pt-16">
       <p className="text-base text-gray-500">Trusted by learners from</p>
       <div className="flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5">
         {srcList.map((src) => (
-          <div key={uniqid()} className="w-20 md:w-28 h-auto">
-            {!isLoaded && (
-              <div className="w-full h-6 sm:h-8 md:h-10 lg:h-12 bg-gray-200 animate-pulse" />
-            )}
-            <img
-              loading="lazy"
-              src={src.img}
-              alt={src.name}
-              onLoad={handleOnload}
-              className={`w-full h-auto ${isLoaded ? "opacity-100" : "opacity-0"}`}
-            />
-          </div>
+          <CompanyLogo key={uniqid()} src={src} />
         ))}
       </div>
     </div>
