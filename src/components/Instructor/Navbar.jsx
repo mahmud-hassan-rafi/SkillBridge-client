ac036c2880451f50ab05cce1fb6f596e9222d915
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-23 18:24:31 +0600

    feat: update Navbar component to display instructor's name and image

--- src/components/Instructor/Navbar.jsx
+++ src/components/Instructor/Navbar.jsx
@@ -1,9 +1,12 @@
 import { assets } from "@assets/assets";
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const instructor = useSelector((state) => state?.auth?.user);
   const navigate = useNavigate();
+
   return (
     <div className="flex items-center justify-between py-4 px-2 sm:px-4 md:px-8 lg:px-24 border-b border">
       <img
@@ -14,11 +17,11 @@ const Navbar = () => {
         onClick={() => navigate("/")}
       />
       <div className="flex gap-4 items-center">
-        <p>Hi! Developer</p>
+        <p>Hi! {instructor?.fullname?.firstname}</p>
         <img
           loading="lazy"
-          src={assets.profile_img}
-          className="profile_image"
+          src={instructor.imageUrl}
+          className="w-10 h-10 rounded-full object-cover object-center"
           alt=""
         />
       </div>
