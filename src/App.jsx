6bfb4584063874d68630a6cc704276e6a491d649
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-25 22:43:07 +0600

    feat: simplify App component by removing ScrollToTop and directly using RouterProvider with loading state

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import "react-quill/dist/quill.snow.css";
 import Loading from "@components/common/Loading";
 import { useLoadUserQuery } from "@features/auth/authApi";
-import ScrollToTop from "@components/common/ScrollToTop";
 import AppRoutes from "@routes/AppRoutes";
+import { RouterProvider } from "react-router-dom";
 
 const App = () => {
   const { isLoading, isFetching } = useLoadUserQuery();
   const loading = isLoading || isFetching;
 
   return !loading ? (
-    <>
-      <ScrollToTop />
-      {/* pass the combined loading flag down so RootRedirect can also guard */}
-      <AppRoutes isLoading={loading} />
-    </>
+    <RouterProvider router={AppRoutes(loading)} />
   ) : (
     <Loading />
   );
