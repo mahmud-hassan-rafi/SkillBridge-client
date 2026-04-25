f477c19226f63601989ad98edf0ae3745113e772
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-04-25 22:52:23 +0600

    feat: add ScrollToTop component to MainLayoutStudent for improved navigation

--- src/layouts/MainLayoutStudent.jsx
+++ src/layouts/MainLayoutStudent.jsx
@@ -1,13 +1,17 @@
+import ScrollToTop from "@components/common/ScrollToTop";
 import Navbar from "@components/Student/Navbar";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const MainLayoutStudent = () => {
   return (
-    <div className="text-default min-w-screen min-h-screen bg-white">
-      <Navbar />
-      <Outlet />
-    </div>
+    <>
+      <ScrollToTop />
+      <div className="text-default min-w-screen min-h-screen bg-white">
+        <Navbar />
+        <Outlet />
+      </div>
+    </>
   );
 };
 
