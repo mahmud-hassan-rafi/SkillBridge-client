c67a38b3b90db0d89ee2fb039f51922f8fb71df7
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-05-09 23:56:02 +0600

    feat: enhance PayButton component with loading state logging and submit type

--- src/features/payment/components/checkoutForm/PayButton.jsx
+++ src/features/payment/components/checkoutForm/PayButton.jsx
@@ -2,9 +2,11 @@ import { useAppContext } from "@hooks/ContextHook";
 
 const PayButton = ({ loading, stripe, courseData }) => {
   const { calculateActualPrice } = useAppContext();
+  console.log("state of loading", loading);
 
   return (
     <button
+      type="submit"
       disabled={!stripe || loading}
       className="w-full bg-black text-white py-2 rounded cursor-pointer"
     >
