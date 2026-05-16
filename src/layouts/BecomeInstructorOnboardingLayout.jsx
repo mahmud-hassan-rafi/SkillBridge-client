860597112e70bf88c78941ca7e3cdbb10867cd41
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-05-16 10:58:01 +0600

    refactor: enhance BecomeInstructorOnboardingLayout with ScrollToTop

--- src/layouts/BecomeInstructorOnboardingLayout.jsx
+++ src/layouts/BecomeInstructorOnboardingLayout.jsx
@@ -1,3 +1,4 @@
+import ScrollToTop from "@components/common/ScrollToTop";
 import Footer from "@components/Instructor/onBoarding/Footer";
 import Navbar from "@components/Instructor/onBoarding/Navbar";
 import React from "react";
@@ -5,13 +6,16 @@ import { Outlet } from "react-router-dom";
 
 const BecomeInstructorOnboardingLayout = () => {
   return (
-    <div className="text-base min-h-screen bg-white ">
-      <Navbar />
-      <div className="px-4 sm:px-8 md:px-16 flex-1 overflow-y-auto">
-        <Outlet />
+    <>
+      <ScrollToTop />
+      <div className="text-base min-h-screen bg-white ">
+        <Navbar />
+        <div className="px-4 sm:px-8 md:px-16 flex-1 overflow-y-auto">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
